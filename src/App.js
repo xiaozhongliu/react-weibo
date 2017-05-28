bef9d79e5f16406ceb07977e4f2a1190c6a2d428
Xiaozhong <xiaozhong.liu@outlook.com>
2017-05-28 21:23:05 +0800

    home page layout

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
+            <div className="app">
+                <img src={logo} className="app-logo" alt="logo"/>
+                <span>React Weibo</span>
+                <button className="app-enter">Enter</button>
             </div>
         )
     }
